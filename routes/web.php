2c78d79adaee76fb9eb83ea94a82511300e527a2
snsu.britt <bmontalvo@ssct.edu.ph>
2024-11-28 17:03:47 +0800

    yield & section added

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('layouts.app');
+    return view('layouts.tasklist');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
